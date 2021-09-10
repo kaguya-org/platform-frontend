1bb3799c4476f91b4c5f869e0c8fddeb90940f8c
Tiago Gonçalves <tiaguin180@gmail.com>
2021-09-10 11:47:40 -0300

    feat: Created user dashboard page

---
+++ src/pages/UserDashboard/index.tsx
@@ -0,0 +1,186 @@
+import { useState } from 'react';
+
+import {
+  FaPlay,
+  FaDiscord,
+  BiDollarCircle,
+  IoArrowRedoSharp,
+  BsPlus,
+  IoIosArrowForward,
+} from 'react-icons/all';
+
+import { SideBar } from '../../components/SideBar';
+import { ProgressBar } from '../../components/ProgressBar';
+
+import {
+  Container,
+  Content,
+  LeftContent,
+  RightContent,
+  Welcome,
+  LastClasseSection,
+  ComunitySection,
+  MyTrailsSection,
+  MyTrailsContainer,
+  MyTrail,
+  OtherTrailsSection,
+  OtherTrailsContainer,
+  OtherTrail,
+} from './styles';
+
+import euImg from '../../assets/images/eu.jpg';
+
+const jsImg =
+  'https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/2048px-Unofficial_JavaScript_logo_2.svg.png';
+
+export function UserDashboard(): JSX.Element {
+  const [progressBar, setProgressBar] = useState(100);
+  return (
+    <Container>
+      <SideBar />
+      <Content>
+        <LeftContent>
+          <Welcome>
+            <img src={euImg} alt="perfil" />
+            <div>
+              <h1>
+                Bem vindo de Volta,
+                <br /> <span>Tiago Gonçalves!</span>
+              </h1>
+              <span> Vamos estudar o que hoje? </span>
+            </div>
+          </Welcome>
+
+          <MyTrailsSection>
+            <header>
+              <h1> Minhas trilhas </h1>
+              <button type="button">Todas as minhas trilhas</button>
+            </header>
+
+            <MyTrailsContainer>
+              <MyTrail>
+                <header>
+                  <img src={jsImg} alt="Js" />
+                  <span>
+                    <IoArrowRedoSharp />
+                    Acessar
+                  </span>
+                </header>
+                <span>Javascript</span>
+
+                <ProgressBar percent={68} />
+              </MyTrail>
+              <MyTrail>
+                <header>
+                  <img src={jsImg} alt="Js" />
+                  <button>
+                    <IoIosArrowForward />
+                  </button>
+                </header>
+                <span>Javascript</span>
+
+                <ProgressBar percent={progressBar} />
+              </MyTrail>
+              <MyTrail>
+                <header>
+                  <img src={jsImg} alt="Js" />
+                  <IoArrowRedoSharp />
+                </header>
+                <span>Javascript</span>
+
+                <ProgressBar percent={4} />
+              </MyTrail>
+            </MyTrailsContainer>
+          </MyTrailsSection>
+        </LeftContent>
+
+        <RightContent>
+          <div>
+            <LastClasseSection>
+              <aside>
+                <div>
+                  <div>
+                    <h1>Continuar de onde parou? </h1>
+                    <p>
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Error eligendi magni inventore sed eveniet impedit. Quidem
+                      dolorem necessitatibus, nemo a sint quo ut maiores,
+                      aspernatur nesciunt pariatur perferendis eum possimus!
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Vero id tenetur, quos non maxime neque obcaecati, quo quod
+                      a ad ab dignissimos ducimus nulla harum, temporibus
+                      recusandae est. Ea, fugit.
+                    </p>
+                  </div>
+                  <img src={jsImg} alt="a" />
+                </div>
+                <span>
+                  <button type="button">
+                    <FaPlay /> Entendendo váriaveis
+                  </button>
+                  <h2>Styled-components</h2>
+                </span>
+              </aside>
+            </LastClasseSection>
+            <ComunitySection>
+              <aside>
+                <h1>Junte-se ou apoie nossa Comunidade</h1>
+                <p>
+                  Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                  Quidem est itaque voluptatum optio amet ex a eius asperiores
+                  nemo nulla? Maxime fugiat sit officia dolores rerum quasi
+                  nihil illum hic!
+                </p>
+                <div>
+                  <button type="button" className="button-discord">
+                    <FaDiscord /> Discord
+                  </button>
+                  <button type="button" className="button-contribute">
+                    <BiDollarCircle /> Contribuir
+                  </button>
+                </div>
+              </aside>
+            </ComunitySection>
+          </div>
+          <OtherTrailsSection>
+            <header>
+              <h1> Outras trilhas </h1>
+              <button type="button">Ver todas as trilhas</button>
+            </header>
+
+            <OtherTrailsContainer>
+              <OtherTrail>
+                <header>
+                  <img src={jsImg} alt="Js" />
+                </header>
+                <span>Styled-components</span>
+                <button type="button"><BsPlus /> Detalhes</button>
+              </OtherTrail>
+              <OtherTrail>
+                <header>
+                  <img src={jsImg} alt="Js" />
+                </header>
+                <span>Styled-components</span>
+                <button type="button"><BsPlus /> Detalhes</button>
+              </OtherTrail>
+              <OtherTrail>
+                <header>
+                  <img src={jsImg} alt="Js" />
+                </header>
+                <span>Styled-components</span>
+                <button type="button"><BsPlus /> Detalhes</button>
+              </OtherTrail>
+              <OtherTrail>
+                <header>
+                  <img src={jsImg} alt="Js" />
+                </header>
+                <span>Styled-components</span>
+                <button type="button"><BsPlus /> Detalhes</button>
+              </OtherTrail>
+            </OtherTrailsContainer>
+          </OtherTrailsSection>
+        </RightContent>
+      </Content>
+    </Container>
+  );
+}
---
+++ src/pages/UserDashboard/styles.ts
@@ -0,0 +1,487 @@
+import styled from 'styled-components';
+
+import backgroundImg from '../../assets/images/background.svg';
+
+export const Container = styled.div`
+  height: 100%;
+  width: 100%;
+
+  display: flex;
+
+  background: url(${backgroundImg}) no-repeat center center;
+  background-size: 100%;
+`;
+
+export const Content = styled.div`
+  width: 100%;
+  padding-left: 36px;
+  margin: 64px 48px 32px 64px;
+  display: flex;
+  align-items: flex-start;
+  gap: 18px;
+
+  section h1 {
+    position: relative;
+    font-size: 24px;
+    font-weight: bold;
+    letter-spacing: 0.6px;
+
+    &:after {
+      content: '';
+      position: absolute;
+
+      bottom: -6px;
+      left: 0;
+      width: 160px;
+      height: 2px;
+      background-color: var(--primary-color);
+    }
+  }
+`;
+
+export const LeftContent = styled.div`
+  max-width: 600px;
+  width: 100%;
+
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+`;
+
+export const RightContent = styled.aside`
+  flex: 1;
+
+  display: grid;
+  > div {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    gap: 16px;
+    margin-bottom: 16px;
+  }
+`;
+
+export const Welcome = styled.section`
+  background: var(--second-background);
+
+  padding: 32px;
+
+  border-radius: 8px;
+
+  display: flex;
+  align-items: flex-start;
+  justify-content: flex-start;
+
+  img {
+    width: 128px;
+    height: 128px;
+    border-radius: 50%;
+    object-fit: cover;
+
+    border: 4px solid var(--third-color);
+
+    padding: 4px;
+  }
+
+  div {
+    display: flex;
+    align-items: flex-start;
+    justify-content: center;
+    flex-direction: column;
+    margin-left: 36px;
+
+    h1 {
+      letter-spacing: 0.8px;
+      font-size: 32px;
+      max-width: 90%;
+
+      &:after {
+        position: initial;
+      }
+
+      span {
+        font-size: 24px;
+        color: var(--third-color);
+        font-family: var(--second-font);
+      }
+    }
+
+    span {
+      margin-top: 16px;
+      font-size: 18px;
+    }
+  }
+`;
+
+export const MyTrailsSection = styled.section`
+  background: var(--second-background);
+
+  padding: 32px;
+
+  border-radius: 8px;
+
+  > header {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    margin-bottom: 32px;
+
+    h1 {
+      &:after {
+        max-width: 80px;
+      }
+    }
+
+    button {
+      font-size: 14px;
+      font-weight: 500;
+
+      color: #c4c4c4;
+      border-bottom: 1px solid #c4c4c4;
+
+      letter-spacing: 0.7px;
+
+      transition: color 0.2s;
+
+      &:hover {
+        color: var(--third-color);
+        border-bottom: 1px solid var(--third-color);
+      }
+    }
+  }
+`;
+
+export const MyTrailsContainer = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  align-items: center;
+  justify-content: center;
+  flex-wrap: wrap;
+  gap: 16px;
+`;
+
+export const MyTrail = styled.div`
+  display: flex;
+  align-items: flex-start;
+  flex-direction: column;
+
+  background: #242731;
+  padding: 24px;
+  border-radius: 8px;
+  max-width: 210px;
+  width: 100%;
+
+  cursor: pointer;
+
+  &:hover {
+    header span {
+      svg path {
+        transition: all 0.2s;
+
+        color: var(--third-color);
+      }
+    }
+  }
+
+  header {
+    display: flex;
+    justify-content: space-between;
+    align-items: flex-start;
+    width: 100%;
+    margin-bottom:0;
+
+    svg {
+      width: 24px;
+        height: 24px;
+    }
+
+    img {
+      width: 72px;
+      height: 72px;
+      border-radius: 8px;
+    }
+
+    span {
+      display: flex;
+      align-items: center;
+      flex-direction: column;
+      gap: 8px;
+
+      color: #c4c4c4;
+      svg {
+        width: 24px;
+        height: 24px;
+      }
+    }
+
+    button {
+      background: #242731;
+      box-shadow: -3px 3px 0 var(--third-color), 3px -3px 0 var(--second-color);
+      padding: 12px;
+      border-radius: 8px;
+
+    }
+  }
+
+  > span {
+    margin: 16px 0 32px;
+  }
+`;
+
+export const LastClasseSection = styled.section`
+  background: var(--second-background);
+
+  padding: 32px;
+
+  border-radius: 8px;
+
+  aside {
+    display: flex;
+    align-items: flex-start;
+    justify-content: space-between;
+    flex-direction: column;
+
+    width: 100%;
+    height: 100%;
+
+    > div {
+      display: flex;
+      align-items: flex-start;
+      justify-content: space-between;
+      width: 100%;
+
+      > div {
+        display: flex;
+        align-items: flex-start;
+        justify-content: center;
+        flex-direction: column;
+        width: 100%;
+
+        p {
+          margin-top: 32px;
+          color: #c4c4c4;
+          max-height: 112px;
+          overflow: hidden;
+          text-overflow: ellipsis;
+          max-width: 380px;
+        }
+      }
+
+      img {
+        width: 64px;
+        height: 64px;
+        border-radius: 8px;
+      }
+    }
+
+    span {
+      display: flex;
+      align-items: center;
+      width: 100%;
+      justify-content: space-between;
+      margin-top: 16px;
+
+      > button {
+        background: #242731;
+
+        cursor: pointer;
+        box-shadow: 3px 3px 0 var(--third-color),
+          -3px -3px 0 var(--second-color);
+        padding: 16px;
+        border-radius: 6px;
+
+        display: flex;
+        place-items: center;
+        gap: 12px;
+
+        transition: all 0.2s;
+        &:hover {
+          filter: brightness(110%);
+        }
+        svg {
+          path {
+            color: var(--primary-color);
+          }
+        }
+      }
+
+      h2 {
+        font-size: 16px;
+        font-weight: 500;
+
+        margin: 16px 0 12px;
+
+        color: #fff;
+      }
+    }
+  }
+`;
+
+export const ComunitySection = styled.section`
+  background: var(--second-background);
+
+  padding: 32px;
+
+  border-radius: 8px;
+
+  aside {
+    height: 100%;
+
+    p {
+      max-width: 400px;
+      width: 90%;
+      margin-top: 32px;
+      color: #c4c4c4;
+    }
+
+    > div {
+      display: flex;
+      align-items: center;
+      gap: 20px;
+
+      margin-top: 32px;
+
+      button {
+        padding: 12px 24px;
+        border-radius: 6px;
+        font-size: 16px;
+
+        display: flex;
+        align-items: center;
+        gap: 8px;
+
+        transition: all 0.2s;
+
+        svg {
+          width: 20px;
+          height: 20px;
+        }
+
+        &:hover {
+          filter: brightness(90%);
+        }
+
+        &.button-discord {
+          background: #5381d3;
+        }
+
+        &.button-contribute {
+          background: #262832;
+
+          color: #59ca7f;
+
+          &:hover {
+            filter: brightness(110%);
+          }
+
+          svg path {
+            color: #59ca7f;
+          }
+        }
+      }
+    }
+  }
+`;
+
+export const OtherTrailsSection = styled.section`
+  background: var(--second-background);
+
+  padding: 32px;
+  margin-bottom: 32px;
+
+  border-radius: 8px;
+
+  > header {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    margin-bottom: 32px;
+
+    h1 {
+      &:after {
+        max-width: 80px;
+      }
+    }
+
+    button {
+      font-size: 14px;
+      font-weight: 500;
+
+      color: #c4c4c4;
+      border-bottom: 1px solid #c4c4c4;
+
+      letter-spacing: 0.7px;
+
+      transition: color 0.2s;
+
+      &:hover {
+        color: var(--third-color);
+        border-bottom: 1px solid var(--third-color);
+      }
+    }
+  }
+`;
+
+export const OtherTrailsContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  align-items: flex-start;
+  justify-content: flex-start;
+  flex-wrap: wrap;
+  gap: 12px;
+`;
+
+export const OtherTrail = styled.div`
+  display: flex;
+  align-items: flex-start;
+  flex-direction: column;
+
+  background: #242731;
+  padding: 24px;
+  border-radius: 8px;
+  max-width: 200px;
+  width: 100%;
+
+  cursor: pointer;
+
+  header {
+    display: flex;
+    justify-content: space-between;
+    width: 100%;
+    margin-bottom: 12px;
+    img {
+      width: 72px;
+      height: 72px;
+      border-radius: 8px;
+    }
+  }
+  button {
+    background: var(--third-color);
+    width: 100%;
+    padding: 12px 16px;
+    text-align: center;
+
+    border-radius: 8px;
+
+    color: var(--text-color);
+    font-size: 14px;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 6px;
+
+    margin-top: 16px;
+    transition: all 0.2s;
+
+    svg {
+      width: 18px;
+      height: 18px;
+    }
+
+    &:hover {
+      filter: brightness(90%);
+    }
+  }
+`;
