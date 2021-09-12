b73e3552744bea7c7f27dec8ce32c7b4f5afd7e2
Tiago Gonçalves <tiaguin180@gmail.com>
2021-09-12 18:42:33 -0300

    feat: Added user playlist router

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,15 +1,17 @@
 import { Switch } from 'react-router-dom';
+import { RouterCustom } from './routerCustom';
 
 import { UserDashboard } from '../pages/UserDashboard';
-import {UserTrail} from '../pages/UserTrail';
-import { RouterCustom } from './routerCustom';
+import { UserTrail } from '../pages/UserTrail';
+import { UserPlaylist } from '../pages/UserPlaylist';
 
 export function Routes() {
   return (
     <Switch>
       {/* User */}
-      <RouterCustom path="/dashboard" component={UserDashboard} />
-      <RouterCustom path="/:trail_name" component={UserTrail} />
+      <RouterCustom path="/dashboard" exact component={UserDashboard} />
+      <RouterCustom path="/:trail_name" exact component={UserTrail} />
+      <RouterCustom path="/:trail_name/:playlist_id" exact component={UserPlaylist} />
 
       {/* Admin */}
     </Switch>
