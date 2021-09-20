9d8c633f13a105bb1c2da6c2bc118b088efbecb3
Tiago Gonçalves <tiaguin180@gmail.com>
2021-09-20 17:27:05 -0300

    feat: Created admin sidebar

---
+++ src/components/AdminSideBar/index.tsx
@@ -0,0 +1,75 @@
+import { useState } from 'react';
+import {
+  MdOndemandVideo,
+  MdPlaylistPlay,
+  RiHistoryFill,
+  IoNotificationsSharp,
+  MdSettings,
+  BsArrowRight,
+  BsArrowLeft,
+  RiLogoutCircleRLine,
+} from 'react-icons/all';
+
+import {
+  Container,
+  Content
+} from './styles';
+
+import euImg from '../../assets/images/eu.jpg';
+import logoImg from '../../assets/images/react.png';
+
+export function AdminSideBar() {
+  return (
+    <Container>
+      <Content>
+        <nav>
+          <div>
+            <img src={logoImg} alt="Eu" />
+            <h1>Minha logo </h1>
+          </div>
+          <ul>
+            <a href="/">
+              <MdPlaylistPlay />
+              <p>Dashboard</p>
+            </a>
+            <a href="/">
+              <MdPlaylistPlay />
+              <p>Sugestões</p>
+            </a>
+
+            <a href="/">
+              <MdOndemandVideo />
+              <p>Criar trilha</p>
+            </a>
+
+            <a href="/">
+              <RiHistoryFill />
+              <p>Histórico da plataforma</p>
+            </a>
+          </ul>
+        </nav>
+        <div>
+          <ul>
+            <a href="/">
+              <IoNotificationsSharp />
+              <p>Notificações</p>
+            </a>
+            <a href="/">
+              <MdSettings />
+              <p>Configurações</p>
+            </a>
+            <a href="/">
+              <RiLogoutCircleRLine />
+              <p> Sair da conta </p>
+            </a>
+          </ul>
+
+          <span>
+            <img src={euImg} alt="Eu" />
+            <p>Tiago Gonçalves de Castro</p>
+          </span>
+        </div>
+      </Content>
+    </Container>
+  );
+}
---
+++ src/components/AdminSideBar/styles.ts
@@ -0,0 +1,158 @@
+import styled from 'styled-components';
+
+export const Container = styled.header`
+  height: 100%;
+
+  display: flex;
+  align-items: center;
+`;
+
+export const Content = styled.section`
+  svg {
+    color: #f9f9f9;
+  }
+
+  display: flex;
+  align-items: flex-start;
+  flex-direction: column;
+  justify-content: space-between;
+
+  max-width: 300px;
+  width: 100%;
+  height: 100%;
+
+  background: var(--second-background);
+  border-radius: 0 12px 12px 0;
+
+  ul li {
+    cursor: pointer;
+  }
+
+  nav {
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    width: 100%;
+
+    width: 100%;
+
+    div {
+      margin-top: 16px;
+
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      padding-left: 24px;
+      flex-direction: row;
+      gap: 16px;
+
+      img {
+        width: 36px;
+        height: 36px;
+        border-radius: 50%;
+      }
+
+      h1 {
+        color: #fff;
+        font-size: 14px;
+      }
+    }
+
+    ul {
+      margin-top: 64px;
+      width: 100%;
+      text-align: left;
+      display: flex;
+      flex-direction: column;
+
+      a {
+        color: #c4c4c4;
+        padding: 16px 20px;
+        border-radius: 8px 0 0 8px;
+
+        display: flex;
+        align-items: center;
+        gap: 16px;
+
+        transition: background 0.3s;
+
+        &:hover {
+          background: var(--primary-background);
+
+          svg path {
+            color: var(--third-color);
+          }
+        }
+
+        svg {
+          width: 20px;
+          height: 20px;
+        }
+      }
+    }
+  }
+
+  div {
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    width: 100%;
+
+    ul {
+      margin-bottom: 16px;
+      text-align: left;
+      width: 100%;
+      display: flex;
+      flex-direction: column;
+
+      a {
+        padding: 16px 20px;
+        display: flex;
+        align-items: center;
+        color: #c4c4c4;
+        gap: 16px;
+
+        transition: color 0.3s;
+
+        &:hover {
+          svg path {
+            color: var(--third-color);
+          }
+        }
+
+        svg {
+          width: 20px;
+          height: 20px;
+        }
+      }
+    }
+
+    span {
+      padding: 16px 0;
+      border-radius: 8px 8px 0 0;
+
+      background: var(--third-color);
+      width: 100%;
+
+      display: flex;
+      align-items: center;
+      gap: 12px;
+
+      p {
+        text-overflow: ellipsis;
+        color: #c4c4c4;
+        width: 70%;
+        padding-right: 12px;
+        white-space: nowrap;
+        overflow: hidden;
+      }
+
+      img {
+        margin-left: 14px;
+        width: 32px;
+        height: 32px;
+        border-radius: 50%;
+      }
+    }
+  }
+`;
