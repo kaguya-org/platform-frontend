2a4f7a1708234cf2c16e8eb468a2633d9b40b996
Tiago Gonçalves <tiaguin180@gmail.com>
2021-09-20 17:28:34 -0300

    feat: Added admin router

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,19 +1,26 @@
 import { Switch } from 'react-router-dom';
 import { RouterCustom } from './routerCustom';
 
+// User routes
 import { UserDashboard } from '../pages/user/UserDashboard';
 import { UserTrail } from '../pages/user/UserTrail';
 import { UserPlaylist } from '../pages/user/UserPlaylist';
 
+// Admin routes
+import { AdminCreateTrail } from '../pages/admin/AdminCreateTrail';
+
 export function Routes() {
   return (
     <Switch>
+      {/* Admin */}
+      <RouterCustom path="/admin" exact>
+
+      </RouterCustom>
+        <RouterCustom path="/admin/trail/create" component={AdminCreateTrail} />
       {/* User */}
       <RouterCustom path="/dashboard" exact component={UserDashboard} />
       <RouterCustom path="/:trail_name" exact component={UserTrail} />
       <RouterCustom path="/:trail_name/:playlist_id" exact component={UserPlaylist} />
-
-      {/* Admin */}
     </Switch>
   );
 };
