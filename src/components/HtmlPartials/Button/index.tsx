9306790fd3b087bcc0b8c1bc0e206a245063ce4f
Tiago Gonçalves <tiaguin180@gmail.com>
2021-09-24 22:56:29 -0300

    style: Changed props position

--- src/components/HtmlPartials/Button/index.tsx
+++ src/components/HtmlPartials/Button/index.tsx
@@ -5,13 +5,18 @@ import {
 } from './styles';
 
 interface ButtonProps extends HTMLAttributes<HTMLButtonElement> {
-  children?: ReactNode;
   className?: string;
+
+  children?: ReactNode;
 }
 
 export function Button({className, children}: ButtonProps) {
+
   return (
-    <ContainerButton type="button" className={`${className}`}>
+    <ContainerButton 
+      type="button" 
+      className={`${className}`}
+    >
       {children}
     </ContainerButton>
   )
