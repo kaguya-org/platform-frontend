e3beb9a04eb27a912fc3e8d1473d1c26cbda8776
Tiago Gonçalves <tiaguin180@gmail.com>
2021-09-25 21:01:57 -0300

    feat: Separeted playlist content on component

---
+++ src/pages/admin/AdminTrail/Partials/PlaylistContainer/index.tsx
@@ -0,0 +1,33 @@
+import { 
+  IoIosArrowForward,
+  HiMenuAlt4
+} from 'react-icons/all';
+
+import {
+  PlaylistContainerStyled
+} from './styles';
+
+type PlaylistContainerType = {
+  data: {
+    id: string;
+    title: string;
+    description: string;
+    index: number;
+  };
+  isDraggable?: boolean;
+};
+
+export function PlaylistContainer({data, isDraggable}: PlaylistContainerType) {
+  return (
+    <PlaylistContainerStyled>
+      <aside>
+        <span>{data.index + 1}</span>
+        <div>
+          <h3>{data.title}</h3>
+          <p>{data.description}</p>
+        </div>
+      </aside>
+      {isDraggable ? <HiMenuAlt4 /> : <IoIosArrowForward />}
+    </PlaylistContainerStyled>
+  );
+}
---
+++ src/pages/admin/AdminTrail/Partials/PlaylistContainer/styles.ts
@@ -0,0 +1,56 @@
+import styled from 'styled-components';
+
+export const PlaylistContainerStyled = styled.div`
+display: flex;
+align-items: center;  
+justify-content: flex-start;
+width: 100%;
+
+> aside {
+  display: flex;
+  align-items: center;
+  gap: 16px;
+  max-width: 90%;
+  width: 100%;
+
+  span {
+    max-width: 48px;
+    width: 100%;
+    height: 48px;
+    background: var(--third-background);
+    border-radius: 50%;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    font-weight: 500;
+  }
+
+  div {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+
+    h3 {
+      font-family: var(--second-font);
+    }
+
+    p {
+      margin-top: 8px;
+      word-break: break-word;
+      color: #c4c4c4;
+    }
+  }
+}
+
+svg {
+  max-width: 36px;
+  width: 100%;
+  height: 36px;
+  
+  path {
+    color: #c4c4c4;
+  }
+}
+`;
