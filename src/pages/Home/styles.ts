ffc810b1d943058423ec00b72bba1aa2dbf98b1b
Tiago Gonçalves <tiaguin180@gmail.com>
2021-10-15 20:01:56 -0300

    feat: Started home page

---
+++ src/pages/Home/styles.ts
@@ -0,0 +1,14 @@
+import styled, { css } from 'styled-components';
+
+export const Container = styled.div``;
+
+export const Content = styled.main``;
+
+export const headerLoginLink = {
+  background: '#FF69F9',
+  color: '#fff',
+  padding: '8px 24px',
+  'border-radius': '8px',
+  'font-weight': '600',
+  'letter-spacing': '0.9px',
+} 
