24996ff2d12513049fb94e34a1797838f2f61cd4
Tiago Gonçalves <tiaguin180@gmail.com>
2021-10-27 16:20:59 -0300

    feat: Created submit login

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,16 +1,70 @@
+import { useRef } from 'react';
+import { FormHandles } from '@unform/core';
+import { FaLock, FaUser } from 'react-icons/all';
+import { Link, useHistory } from 'react-router-dom';
+import * as Yup from 'yup';
+
 import { Header } from '../../components/Header';
 import { Button } from '../../components/HtmlPartials/Button';
 import { Input } from '../../components/HtmlPartials/Input';
-import { FaLock, FaUser } from 'react-icons/all';
-import { Link } from 'react-router-dom';
+
+import { LoginParams } from '../../services/apiParams';
+
+import { getValidationErrors } from '../../utils/getValidationErrors';
+import { useAuth } from '../../hooks/useAuth';
 
 import {
   Container,
   Content,
   FormTag
 } from './styles';
+import { useLoading } from '../../hooks/useLoading';
+import { Loading } from '../../components/Loading';
 
 export function Login() {
+  const history = useHistory();
+  const loginFormRef = useRef<FormHandles>(null);
+  const { signIn } = useAuth();
+  const loading = useLoading(false);
+
+  async function handleSubmitLogin(data: LoginParams) {
+    loading.startLoading();
+
+    try {
+      loginFormRef.current?.setErrors({});
+
+      const schema = Yup.object().shape({
+        email: Yup.string().email('E-mail inválido').required('Email obrigatório'),
+        password: Yup.string().required('Senha obrigatória'),
+      });
+      
+      await schema.validate(data, {
+        abortEarly: false
+      });
+
+      await signIn(data);
+
+      loading.stopLoading();
+
+      history.push('/dashboard');
+
+    } catch(error) {
+      loading.stopLoading();
+
+      if(error instanceof Yup.ValidationError) {
+        const errors = getValidationErrors(error);
+        
+        loginFormRef.current?.setErrors(errors);
+
+        return;
+      }
+
+      loginFormRef.current?.setErrors({
+        email: 'Cheque as credenciais'
+      });
+    }
+  };
+  
   return (
     <Container>
       <Header />
@@ -21,7 +75,7 @@ export function Login() {
         </div>
         <section>
           <h1>Logar na plataforma</h1>
-          <FormTag>
+          <FormTag ref={loginFormRef} onSubmit={handleSubmitLogin}>
             <div>
               <Input 
                 name="email" 
@@ -38,7 +92,12 @@ export function Login() {
               <button>Redefina agora</button>
             </span>
             </div>
-            <Button>Entrar</Button>
+            <Button type="submit" disabled={loading.state} >
+              {loading.state 
+                ? <Loading type="circle"/> 
+                : 'Entrar'
+              }
+            </Button>
           </FormTag>
         </section>
       </Content>
