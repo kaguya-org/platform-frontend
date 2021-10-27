70ce8ded0e29cf07d4b77c8a87bd1ff7d3cb03e4
Tiago Gonçalves <tiaguin180@gmail.com>
2021-10-27 16:20:12 -0300

    feat: Added Form in styled tag

--- src/pages/Login/styles.ts
+++ src/pages/Login/styles.ts
@@ -1,5 +1,7 @@
 import styled from 'styled-components';
 
+import { Form } from '@unform/web';
+
 export const Container = styled.div`
   width: 100%;
   height: 100%;
@@ -52,7 +54,7 @@ export const Content = styled.main`
   }
 `;
 
-export const FormTag = styled.form`
+export const FormTag = styled(Form)`
   > div {
     display: flex;
     flex-direction: column;
