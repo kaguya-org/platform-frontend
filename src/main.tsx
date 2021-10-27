305f32490a941827f2d489b21566b82049f678cf
Tiago Gonçalves <tiaguin180@gmail.com>
2021-10-27 14:31:22 -0300

    feat: Added auth provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,14 @@
-import { render } from 'react-dom';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-import {App} from './App';
+import { App } from './App';
+import { AuthProvider } from './contexts/auth';
 
-const rootElement = document.getElementById('root');
-render(<App />, rootElement);
+ReactDOM.render(
+  <React.StrictMode>
+    <AuthProvider>
+    <App />
+    </AuthProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
