30f3d12059841e75a9e76f813020342791e5547c
Tiago Gonçalves <tiaguin180@gmail.com>
2021-10-27 14:32:07 -0300

    feat: Added unform to form

--- src/components/HtmlPartials/Input/index.tsx
+++ src/components/HtmlPartials/Input/index.tsx
@@ -1,7 +1,11 @@
-import { HTMLAttributes, ReactElement, ReactNode } from 'react';
+import { HTMLAttributes, ReactElement, useRef, useEffect } from 'react';
+
+import { useField } from '@unform/core'
+
 import { 
   Container,
-  Content
+  Content,
+  InputError
 } from './styles';
 
 interface InputProps extends HTMLAttributes<HTMLInputElement> {
@@ -20,14 +24,34 @@ export function Input({
   type, 
   title, 
   inputType, 
-  defaultValue, 
   className,
   icon
 }: InputProps) {
+  const inputRef = useRef(null);
+
+  const { fieldName, defaultValue, registerField, error } = useField(name);
+
+  useEffect(() => {
+    registerField({
+      name: fieldName,
+      ref: inputRef,
+      getValue: ref => {
+        return ref.current.value
+      },
+      setValue: (ref, value) => {
+        ref.current.value = value
+      },
+      clearValue: ref => {
+        ref.current.value = ''
+      },
+    })
+  }, [fieldName, registerField]);
+
   return (
     <Container 
       htmlFor={name} 
       className={`labelInput ${className}`}
+      isError={!!error}
     >
       {icon && inputType !== 'textarea' && icon }
       <Content>
@@ -37,6 +61,7 @@ export function Input({
             name={name}
             placeholder=" "
             defaultValue={defaultValue} 
+            ref={inputRef}
           />
         ) : (
           <>
@@ -46,11 +71,13 @@ export function Input({
               name={name}
               placeholder=" "
               defaultValue={defaultValue} 
+              ref={inputRef}
             />
           </>
         )}
         {title && <span>{title}</span>}
       </Content>
+      {error && <InputError title={error} />}
     </Container>
   )
 }
