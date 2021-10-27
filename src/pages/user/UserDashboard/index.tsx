c3f610a6bc2ee2619ebd3bd9c912002fa77f6618
Tiago Gonçalves <tiaguin180@gmail.com>
2021-10-27 16:21:36 -0300

    fix: Changed import to '../..

--- src/pages/user/UserDashboard/index.tsx
+++ src/pages/user/UserDashboard/index.tsx
@@ -9,9 +9,9 @@ import {
   IoIosArrowForward,
 } from 'react-icons/all';
 
-import { SideBar } from 'components/SideBar';
-import { ProgressBar } from 'components/ProgressBar';
-import { Button } from 'components/HtmlPartials/Button';
+import { SideBar } from '../../../components/SideBar';
+import { ProgressBar } from '../../../components/ProgressBar';
+import { Button } from '../../../components/HtmlPartials/Button';
 
 import {
   Container,
@@ -29,7 +29,7 @@ import {
   OtherTrail,
 } from './styles';
 
-import euImg from 'assets/images/eu.jpg';
+import euImg from '../../../assets/images/eu.jpg';
 
 const jsImg =
   'https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/2048px-Unofficial_JavaScript_logo_2.svg.png';
