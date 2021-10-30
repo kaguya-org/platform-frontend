07d37f0715042d2c44ed16bfc097cfe472de44ad
Tiago Gonçalves <tiaguin180@gmail.com>
2021-10-29 22:02:35 -0300

    feat: Added trail and playlist routes

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,19 +1,51 @@
-import axios, { AxiosRequestConfig, AxiosResponse } from 'axios';
+import axios, { AxiosError, AxiosRequestConfig, AxiosResponse } from 'axios';
 
 import { 
   LoginParams,
   CreateUserParams,
-  CreateUserByAdminParams
+  CreateUserByAdminParams,
+  CreateTrailParams,
+  UpdateTrailParams,
+  DeleteTrailParams,
+  CreatePlaylistParams
 } from './apiParams';
 
 import {
   LoginResponse,
   CreateUserResponse,
+  CreateTrailResponse,
+  UpdateTrailAvatarResponse,
+  ListAllTrailsResponse,
+  ListAllPlaylistsByTrailResponse,
+  CreatePlaylistResponse,
 } from './apiResponse';
 
 export const baseApi = axios.create({
-  baseURL:  'https://slinked-test.herokuapp.com'
+  baseURL: 'https://slinked-test.herokuapp.com',
 });
+
+const adminResource = '/sub-admins';
+
+const adminTrailApi = {
+  create: (data: CreateTrailParams, config?: AxiosRequestConfig): Promise<AxiosResponse<CreateTrailResponse>> => {
+    return baseApi.post(`${adminResource}/trails`, data, config);
+  },
+  update: (data: UpdateTrailParams, config?: AxiosRequestConfig): Promise<AxiosResponse> => {
+    return baseApi.put(`${adminResource}/trails`, data, config);
+  },
+  updateAvatar: (data: FormData, config?: AxiosRequestConfig): Promise<AxiosResponse<UpdateTrailAvatarResponse>> => {
+    return baseApi.patch(`${adminResource}/trails/avatar`, data, config);
+  },
+  delete: (data: DeleteTrailParams, config?: AxiosRequestConfig): Promise<AxiosResponse> => {
+    return baseApi.patch(`${adminResource}/trails?trail_id=${data.trail_id}`, config);
+  },
+};
+
+const adminPlaylistApi = {
+  create: (data: CreatePlaylistParams, config?: AxiosRequestConfig): Promise<AxiosResponse<CreatePlaylistResponse>> => {
+    return baseApi.post(`${adminResource}/playlists`, data, config);
+  }
+}
 
 export const api = {
   authenticate: {
@@ -24,11 +56,29 @@ export const api = {
   user: {
     create: (data: CreateUserParams, config?: AxiosRequestConfig): Promise<AxiosResponse<CreateUserResponse>> => {
       return baseApi.post('/users', data, config);
-    },
+    }
   },
   admin: {
     createUser: (data: CreateUserByAdminParams, config?: AxiosRequestConfig): Promise<AxiosResponse> => {
       return baseApi.post('/sub-admins/users', data, config);
+    },
+    trail: {
+      ...adminTrailApi
+    },
+    playlist: {
+      ...adminPlaylistApi
+    }
+  },
+  global: {
+    trail: {
+      listAll: (): Promise<AxiosResponse<ListAllTrailsResponse[]>> => {
+        return baseApi.get('/trails/list-all');
+      },
+    },
+    playlist: {
+      listAllByTrail: (): Promise<AxiosResponse<ListAllPlaylistsByTrailResponse[]>> => {
+        return baseApi.get('/playlists/trail-list-all');
+      }
     }
   }
 };
