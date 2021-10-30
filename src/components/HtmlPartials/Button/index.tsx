197b2ebcffa2fe673305681c703b72327a7f1db6
Tiago Gonçalves <tiaguin180@gmail.com>
2021-10-29 21:57:08 -0300

    feat: Added loading in button

--- src/components/HtmlPartials/Button/index.tsx
+++ src/components/HtmlPartials/Button/index.tsx
@@ -1,21 +1,31 @@
 import { ReactNode } from 'react';
+import { Loading } from '../..';
 
 import { 
   ContainerButton 
 } from './styles';
 
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-
   children?: ReactNode;
+  isLoading?: boolean;
+  loadingSize?: {
+    width?: string;
+    height?: string;
+  }
 }
 
-export function Button({children, ...rest}: ButtonProps) {
+export function Button({children, isLoading, loadingSize, ...rest}: ButtonProps) {
 
   return (
-    <ContainerButton 
+    <ContainerButton
+      disabled={isLoading}
       {...rest}
     >
-      {children}
+      {isLoading ? (
+        <Loading size={loadingSize} type="circle" />         
+      ) : (
+        children
+      )}
     </ContainerButton>
   )
 }
