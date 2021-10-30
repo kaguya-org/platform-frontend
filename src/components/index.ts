dcd41b1166511673ba3e0d27685f778fdacead83
Tiago Gonçalves <tiaguin180@gmail.com>
2021-10-29 22:03:58 -0300

    feat: Expoting all component in one file

---
+++ src/components/index.ts
@@ -0,0 +1,11 @@
+export { Header } from './Header';
+export { Loading } from './Loading';
+export { ProgressBar } from './ProgressBar';
+export { SideBar } from './SideBar';
+export { Tooltip } from './Tooltip';
+export { AdminSideBar } from './AdminSideBar';
+export { BoxProgressBarStep } from './BoxProgressBarStep';
+export { Button } from './HtmlPartials/Button';
+export { Input } from './HtmlPartials/Input';
+export { InputFile } from './HtmlPartials/InputFile';
+export { UserPhoto } from './UserPhoto';
