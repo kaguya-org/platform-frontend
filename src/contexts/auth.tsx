5a6c34bde18cb008414fbb2b4f6eee5f4402201e
Tiago Gonçalves <tiaguin180@gmail.com>
2021-10-29 22:00:16 -0300

    feat: Added verify if api has error

--- src/contexts/auth.tsx
+++ src/contexts/auth.tsx
@@ -1,11 +1,13 @@
-import { createContext, ReactNode, useEffect, useState } from 'react';
+import { AxiosError } from 'axios';
+import { createContext, ReactNode, useCallback, useEffect, useState } from 'react';
+import { useHistory } from 'react-router';
 import { api, baseApi } from '../services/api';
 import { LoginParams } from '../services/apiParams';
 import { User, LoginResponse } from '../services/apiResponse';
 
 type AuthContextData = {
   user: User | null;
-  token: string;
+  token: string | null;
   signIn(credentials: LoginParams): Promise<void>;
   signOut(): void;
 }
@@ -17,14 +19,12 @@ type AuthProviderProps = {
 export const AuthContext = createContext<AuthContextData>({} as AuthContextData);
 
 export function AuthProvider({ children }: AuthProviderProps) {
+  const slinkedToken = '@slikend:token';
+  const getToken = localStorage.getItem(slinkedToken);
+
   const [user, setUser] = useState<User | null>(null);
-  const [token, setToken] = useState('');
-
-  const slinkedToken = '@slikend:token'
-
-  useEffect(() => {
-
-  }, []);
+  const [token, setToken] = useState(getToken);
+  const history = useHistory();
 
   async function signIn(credentials: LoginParams): Promise<void> {
     const response = await api.authenticate.login(credentials);
@@ -33,17 +33,44 @@ export function AuthProvider({ children }: AuthProviderProps) {
 
     localStorage.setItem(slinkedToken, token);
 
-    baseApi.defaults.headers.common.authorization = `Bearer ${token}`;
+    baseApi.defaults.headers.common.Authorization = `Bearer ${token}`;
 
     setUser(user);
     setToken(token);
   }
 
-  function signOut() {
-    localStorage.removeItem(slinkedToken);
+  const signOut = useCallback(() => {
+    history.push('/');
 
+    localStorage.removeItem(slinkedToken);
+    
     setUser(null);
-  }
+    
+    return;
+  }, [history]);
+
+  useEffect(() => {
+    baseApi.interceptors.response.use(undefined, (error: AxiosError) => {
+      if(!error.response?.status && (error.response?.statusText === 'xhr' || 'preflight')) {
+        // window.location.reload();
+        // return false;
+        signOut();
+
+        return;
+      };
+
+      if(error.response?.status === 401 || error.response?.statusText === '401 Unauthorized') {
+        signOut();
+        return; 
+      }
+
+      return Promise.reject(error);
+    });
+
+    baseApi.defaults.headers.common.authorization = `Bearer ${token}`;
+
+  }, [history, token, baseApi]);
+
 
   return (
     <AuthContext.Provider
