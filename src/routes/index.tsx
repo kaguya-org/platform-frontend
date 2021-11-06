d574e86097c517480db8623aef36e7238c1f1e4b
Tiago Gonçalves <tiaguin180@gmail.com>
2021-11-06 13:37:03 -0300

    feat: Otimized routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,15 +1,17 @@
 import { Switch } from 'react-router-dom';
-import { RouterCustom } from './routerCustom';
+import { RouterCustom, RouterCustomProps } from './routerCustom';
 
 // User routes
 import { UserDashboard } from '../pages/user/UserDashboard';
 import { UserTrail } from '../pages/user/UserTrail';
 import { UserPlaylist } from '../pages/user/UserPlaylist';
+import { UserProfile } from '../pages/user/UserProfile';
 
 // Admin routes
 import { AdminCreateTrail } from '../pages/admin/AdminCreateTrail';
 import { AdminTrail } from '../pages/admin/AdminTrail';
 import { AdminPlaylist } from '../pages/admin/AdminPlaylist';
+import { AdminDashboard } from '../pages/admin/AdminDashboard';
 
 // Global routes
 import { Login } from '../pages/Login';
@@ -17,23 +19,63 @@ import { Register } from '../pages/Register';
 import { Home } from '../pages/Home';
 
 export function Routes() {
+  const adminRepeatProps: RouterCustomProps = {
+    role: {
+      permission: 1,
+      name: 'sub-admin'
+    },
+    exact: true,
+    isPrivate: true,
+  };
+  
   return (
     <Switch>
       {/* Admin */}
-      {/* <RouterCustom path="/admin" exact /> */}
+      <RouterCustom 
+        path="/admin"
+        {...adminRepeatProps}
+        component={AdminDashboard} 
+      />
+      <RouterCustom 
+        path="/admin/trail/create"
+        {...adminRepeatProps}
+        component={AdminCreateTrail} 
+      />
+      <RouterCustom 
+        path="/admin/trail/:trail_id"
+        {...adminRepeatProps}
+        component={AdminTrail} 
+      />
+      <RouterCustom 
+        path="/admin/playlist/:playlist_id"
+        {...adminRepeatProps}
+        component={AdminPlaylist} 
+      />
 
-      <RouterCustom path="/admin/trail/create" isAdmin isPrivate exact component={AdminCreateTrail} />
-      <RouterCustom path="/admin/trail/:trail_id" isAdmin isPrivate exact component={AdminTrail} />
-      <RouterCustom path="/admin/trail/:trail_id/playlist" isAdmin isPrivate exact component={AdminPlaylist} />
+       {/* <RouterCustom 
+        path="/admin" role={{
+          permission: 1,
+          name: 'sub-admin'
+        }}
+        exact
+        component={AdminDashboard}
+      >
+        <RouterCustom path="/trail">
+          <RouterCustom path="/create" component={AdminCreateTrail} />
+          <RouterCustom path="/:trail_id" component={AdminTrail} />
+          <RouterCustom path="/:playlist_id" component={AdminPlaylist} />
+        </RouterCustom>
+      </RouterCustom> */}
       
       {/* User */}
+      <RouterCustom path="/profile" ifAuthenticated isPrivate exact component={UserProfile} />
       <RouterCustom path="/dashboard" ifAuthenticated isPrivate exact component={UserDashboard} />
-      <RouterCustom path="/trail/:trail_name" ifAuthenticated isPrivate exact component={UserTrail} />
+      <RouterCustom path="/trail/:trail_id" ifAuthenticated isPrivate exact component={UserTrail} />
       <RouterCustom path="/trail/:trail_name/:playlist_id" ifAuthenticated isPrivate exact component={UserPlaylist} />
 
       {/* Global */}
-      <RouterCustom path="/login" component={Login} />
-      <RouterCustom path="/register" component={Register} />
+      <RouterCustom path="/login" ifLoggedNotEnter component={Login} />
+      <RouterCustom path="/register" ifLoggedNotEnter component={Register} />
       <RouterCustom path="/" component={Home} exact/>
     
     </Switch>
