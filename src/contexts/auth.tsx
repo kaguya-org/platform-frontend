ed35ee0ee019c659a3fed21e8227940915a7bab2
Tiago Gonçalves <tiaguin180@gmail.com>
2021-11-06 13:33:26 -0300

    feat: Added default auth and others alters

--- src/contexts/auth.tsx
+++ src/contexts/auth.tsx
@@ -1,12 +1,15 @@
-import { createContext, ReactNode, useCallback, useEffect, useState } from 'react';
-import { api } from '../services/api';
+import { createContext, ReactNode, useEffect, useState } from 'react';
+import { useHistory } from 'react-router';
+import { api, baseApi, slinkedApiToken } from '../services/api';
 import { LoginParams } from '../services/apiParams';
-import { User } from '../services/apiResponse';
+import { User, LoginResponse} from '../services/apiResponse';
 
 type AuthContextData = {
   user: User | null;
   token: string | null;
-  signIn(credentials: LoginParams): Promise<void>;
+  tokenIsValid: boolean | undefined;
+
+  signIn(credentials: LoginParams): Promise<LoginResponse | undefined>;
   signOut(): void;
 }
 
@@ -17,36 +20,66 @@ type AuthProviderProps = {
 export const AuthContext = createContext<AuthContextData>({} as AuthContextData);
 
 export function AuthProvider({ children }: AuthProviderProps) {
-  const slinkedToken = '@slinked:token';
-  const getToken = localStorage.getItem(slinkedToken);
-
+  const history = useHistory();
+  
   const [user, setUser] = useState<User | null>(null);
-  const [token, setToken] = useState(getToken);
+  const [token, setToken] = useState<string | null>(null);
+  const [tokenIsValid, setTokenIsValid] = useState(true);
 
-  async function signIn(credentials: LoginParams): Promise<void> {
+  useEffect(() => {
+    const getToken = localStorage.getItem(slinkedApiToken);
+    setToken(getToken);
+
+    const getUser = localStorage.getItem('user');
+
+    baseApi.defaults.headers.common['Authorization'] = `Bearer ${getToken}`;
+
+    api.user.token.validate().then(response => {
+      setTokenIsValid(response.data.validated);
+    });
+
+    setUser(getUser ? JSON.parse(getUser) : null);
+  }, []);
+
+  async function signIn(credentials: LoginParams): Promise<LoginResponse | undefined> {
     const response = await api.authenticate.login(credentials);
 
     const { token, user } = response.data;
 
-    localStorage.setItem(slinkedToken, token);
+    const userMapped = {
+      ...user,
+      user_roles: [{
+        role: user.user_roles[0].role,
+      }],
+    };
 
-    setUser(user);
+    localStorage.setItem(slinkedApiToken, token);
+
+    baseApi.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+
     setToken(token);
+    setUser(userMapped);
+    setTokenIsValid(true);
+
+    const responseModified = {
+      user: userMapped,
+      token,
+    };
+
+    return responseModified || undefined;
   }
 
-  function signOut() {
-    localStorage.removeItem(slinkedToken);
+  function signOut(redirect?: boolean) {
+    localStorage.removeItem(slinkedApiToken);
 
-    setUser(null);
-
-    window.location.href = '/';
-    
-    return;
+    if(redirect) {
+      history.push('/login');
+    }
   }
 
   return (
     <AuthContext.Provider
-      value={{signIn, signOut, user, token}}
+      value={{signIn, signOut, user, token, tokenIsValid}}
     >
       {children}
     </AuthContext.Provider>
