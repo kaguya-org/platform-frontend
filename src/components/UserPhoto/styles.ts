ff2593be992b175df0815d2b60cfdf34f381346a
Tiago Gonçalves <tiaguin180@gmail.com>
2021-11-14 14:31:07 -0300

    feat: Added new props default in css

--- src/components/UserPhoto/styles.ts
+++ src/components/UserPhoto/styles.ts
@@ -4,11 +4,18 @@ export const Container = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+
   background: linear-gradient(to right bottom, var(--primary-color), var(--third-color));
   padding: 4px;
+  border-radius: 50%;
+
+  width: min-content;
 `;
 
 export const AvatarImage = styled.img`
-  border:4px solid var(--third-background);
+  border: 4px solid var(--third-background);
+
   object-fit: cover;
+
+  border-radius: 50%;
 `;
