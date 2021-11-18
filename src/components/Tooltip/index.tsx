80e161a3e7b2d8c794dc4c999b62411c8b04a549
Tiago Gonçalves <tiaguin180@gmail.com>
2021-11-18 15:55:24 -0300

    feat: Recieved icon and outhers props

--- src/components/Tooltip/index.tsx
+++ src/components/Tooltip/index.tsx
@@ -1,5 +1,6 @@
 import { ReactNode } from 'react';
 import { BiErrorCircle } from 'react-icons/all';
+import { IconType } from 'react-icons';
 
 import { Container, TooltipType } from './styles';
 
@@ -18,12 +19,26 @@ interface TooltipProps {
   children?: ReactNode;
 
   type?: TooltipType;
+  showIcon?: boolean;
+  icon?: IconType;
 }
 
-export function Tooltip({ title, type = 'normal', children, ...rest}: TooltipProps) {
+export function Tooltip({ 
+  title, 
+  type = 'normal', 
+  icon: Icon, 
+  children,
+  showIcon = true,
+  ...rest}: TooltipProps) {
   return (
     <Container type={type} {...rest.containerProps}>
-      <BiErrorCircle {...rest.svgProps} />
+      {showIcon && (
+        !Icon ? (
+          <BiErrorCircle {...rest.svgProps} />
+        ) : (
+          <Icon />
+        )
+      )}
       {children}
       <span>{title}</span>
     </Container>
