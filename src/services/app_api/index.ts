dfe2838eac62fcc401bcd620b88b6c1ffc18524c
Tiago Gonçalves <tiaguin180@gmail.com>
2021-11-28 14:33:23 -0300

    feat: Exporting api response and params types

--- src/services/app_api/index.ts
+++ src/services/app_api/index.ts
@@ -1,3 +1,7 @@
 export * as admin from './admin';
 export * as user from './user';
 export * as global from './global';
+
+export * as UserType from './user/types'
+export * as AdminType from './user/types'
+export * as GlobalType from './user/types'
---
+++ src/services/app_api/user/types.ts
@@ -0,0 +1,7 @@
+export * from './geral/params';
+export * from './trail/params';
+// export * from './playlist/params';
+
+export * from './geral/response';
+export * from './trail/response';
+// export * from './playlist/response';
