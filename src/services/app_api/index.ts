33e866e67bdb01abfe5b1a93290fc5cde77098cf
Tiago Gonçalves <tiaguin180@gmail.com>
2021-11-29 11:07:38 -0300

    fix: importing correct files

--- src/services/app_api/index.ts
+++ src/services/app_api/index.ts
@@ -2,6 +2,6 @@ export * as admin from './admin';
 export * as user from './user';
 export * as global from './global';
 
-export * as UserType from './user/types'
-export * as AdminType from './user/types'
-export * as GlobalType from './user/types'
+export * as UserType from './user/types';
+export * as AdminType from './admin/types';
+export * as GlobalType from './global/types';
