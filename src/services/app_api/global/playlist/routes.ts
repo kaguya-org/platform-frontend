a5b2fdaa0cb008704d2dbf64aa547a8922631895
Tiago Gonçalves <tiaguin180@gmail.com>
2021-12-08 15:43:03 -0300

    feat: Create get info playlist router

--- src/services/app_api/global/playlist/routes.ts
+++ src/services/app_api/global/playlist/routes.ts
@@ -6,14 +6,21 @@ import {
 
 import {
   ListAllPlaylistByTrailParams,
+  ShowPlaylistParams
 } from './params';
 
 import {
   ListAllPlaylistsByTrailResponse,
+  ShowPlaylistResponse
 } from './response';
 
 export const playlist = {
   listAllByTrail: (data: ListAllPlaylistByTrailParams): Promise<AxiosResponse<ListAllPlaylistsByTrailResponse[]>> => {
     return baseApi.get(`/playlists/trail-list-all?trail_id=${data.trail_id}`);
+  },
+  getInfo: (data: ShowPlaylistParams): Promise<AxiosResponse<ShowPlaylistResponse>> => {
+    return baseApi.get('/playlists/show', {
+      params: data
+    })
   }
 }
