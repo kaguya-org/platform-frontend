8cec180777b97c48bf8bf5100940b5cba38d64cc
Tiago Gonçalves <tiaguin180@gmail.com>
2021-12-08 15:44:54 -0300

    feat: Added params and response in show playlist types

--- src/services/app_api/global/playlist/response.ts
+++ src/services/app_api/global/playlist/response.ts
@@ -9,3 +9,14 @@ export type ListAllPlaylistsByTrailResponse = {
   created_at: string,
   updated_at: string;
 };
+
+export type ShowPlaylistResponse = {
+  id: string;
+  name: string;
+  description: string;
+  trail_id: string;
+  avatar: null | string;
+  avatar_url: null | string;
+  created_at: string;
+  updated_at: string;
+}
