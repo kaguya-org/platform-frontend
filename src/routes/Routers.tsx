adbe3fa9dab247d1d6d931a4111c319606c2b27f
Tiago Gonçalves <tiaguin180@gmail.com>
2022-02-19 15:00:21 -0300

    refactor: Updated routers to best perf

---
+++ src/routes/Routers.tsx
@@ -0,0 +1,87 @@
+import RouterDom, { 
+  Route, 
+  Navigate, 
+  Routes,
+  useMatch,
+  BrowserRouter,
+  Outlet
+} from 'react-router-dom';
+
+import { 
+  Login,
+  Register,
+  User,
+} from '@/pages';
+import { useAuth } from '@/hooks';
+import { Loading } from '@/components';
+
+export type PrivateRouteProps = {
+  redirect_to?: string;
+}
+
+export function PrivateRoute({
+  redirect_to = '/login',
+}: PrivateRouteProps) {
+  const { tokenIsValid } = useAuth();
+  
+  if(!tokenIsValid) {
+    return <Navigate to={redirect_to} />
+  }
+  
+  // TODO - validate permission
+  // {}
+
+  return  <Outlet />
+}
+
+export function PublicRoute({
+  redirect_to = '/user',
+}: PrivateRouteProps) {
+  const { tokenIsValid } = useAuth();
+  
+  if(tokenIsValid) {
+    return <Navigate to={redirect_to} />
+  }
+
+  // TODO - validate permission
+  // {}
+
+  return  <Outlet />
+}
+
+export function Routers() {
+  const { tokenIsValid } = useAuth();
+ 
+  return (
+    <Routes>
+      {/* Publics */}
+      <Route element={<PublicRoute />}>
+        <Route element={<Login />} path="/" />
+        <Route element={<Login />} path="/login" />
+        <Route element={<Register />} path="/register" />
+      </Route>
+
+      {/* Users private */}
+      <Route element={<PrivateRoute />}>
+        <Route element={<User.Dashboard />} path="/user" />
+      </Route>
+
+      <Route element={<Navigate to={tokenIsValid ? '/user' : '/login'} />} path="*" />
+
+      {/* Admin private */}
+      {/* <Route element={<Admin.CreateTrail />} path="/admin" /> 
+      <Route element={<Admin.Playlist />} path="/admin/playlist/:playlist_id" />
+
+      <Route element={<Admin.Trail />} path="/admin/trail/:trail_id" />
+      <Route element={<Admin.CreateTrail />} path="/admin/trail/create" /> */}
+
+      {/* User */}
+  {/* 
+      <Route element={<User.Playlist />} path="/user/playlist/:playlist_id" />
+      <Route element={<User.Trail />} path="/user/trail/:trail_id" />
+      <Route element={<User.Playlist />} path="/user/trail/:trail_id/:playlist_id" /> */}
+
+
+    </Routes>
+  );
+}
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,60 +1,22 @@
-import { Switch } from 'react-router-dom';
-import { RouterCustom, RouterCustomProps } from './routerCustom';
+import { BrowserRouter } from 'react-router-dom';
 
-// User routes
-import { Dashboard as UserDashboard } from '../pages/user/Dashboard';
-import { Trail as UserTrail } from '../pages/user/Trail';
-import { Playlist as UserPlaylist } from '../pages/user/Playlist';
+import { Loading } from '@/components';
+import { useAuth } from '@/hooks';
 
-// Admin routes
-import { CreateTrail as AdminCreateTrail } from '../pages/admin/CreateTrail';
-import { Trail as AdminTrail } from '../pages/admin/Trail';
-import { Playlist as AdminPlaylist } from '../pages/admin/Playlist';
+import { Routers } from './Routers';
 
-// Global routes
-import { Login } from '../pages/Login';
-import { Register } from '../pages/Register';
-import { Home } from '../pages/Home';
+export function AppRoutes() {
+  const { loading_page } = useAuth();
 
-export function Routes() {
-  const adminRepeatProps: RouterCustomProps = {
-    role: {
-      permission: 1,
-      name: 'sub-admin'
-    },
-    exact: true,
-    isPrivate: true,
-  };
-  
   return (
-    <Switch>
-      {/* Admin */}
-      <RouterCustom 
-        path="/admin/trail/create"
-        {...adminRepeatProps}
-        component={AdminCreateTrail} 
-      />
-      <RouterCustom 
-        path="/admin/trail/:trail_id"
-        {...adminRepeatProps}
-        component={AdminTrail} 
-      />
-      <RouterCustom 
-        path="/admin/playlist/:playlist_id"
-        {...adminRepeatProps}
-        component={AdminPlaylist} 
-      />
-
-      {/* User */}
-      <RouterCustom path="/dashboard" ifAuthenticated isPrivate exact component={UserDashboard} />
-      <RouterCustom path="/trail/:trail_id" ifAuthenticated isPrivate exact component={UserTrail} />
-      <RouterCustom path="/trail/:trail_id/:playlist_id" ifAuthenticated isPrivate exact component={UserPlaylist} />
-
-      {/* Global */}
-      <RouterCustom path="/login" ifLoggedNotEnter component={Login} />
-      <RouterCustom path="/register" ifLoggedNotEnter component={Register} />
-      <RouterCustom path="/" component={Home} exact/>
-    
-    </Switch>
+    <>
+      {loading_page ? (
+        <Loading size={'72px'} />
+      ) : (
+        <BrowserRouter>
+          <Routers /> 
+        </BrowserRouter>
+      )}
+    </>
   );
-};
+}
