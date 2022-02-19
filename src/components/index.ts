670c50db5689e9184939c01da09dfa4b292a7824
Tiago Gonçalves <tiaguin180@gmail.com>
2022-02-19 14:58:48 -0300

    style: Organizad imports

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,15 +1,24 @@
+// Global
 export { Header } from './HtmlPartials/Header';
 export { Loading } from './Loading';
 export { ProgressBar } from './ProgressBar';
-export { SideBar } from './SideBar';
+export { Navbar } from './Navbar';
 export { Tooltip } from './Tooltip';
-export { AdminSideBar } from './AdminSideBar';
 export { BoxProgressBarStep } from './BoxProgressBarStep';
+
 export { Button } from './HtmlPartials/Button';
 export { Input } from './HtmlPartials/Input';
 export { InputFile } from './HtmlPartials/InputFile';
 export { InputCheckbox } from './HtmlPartials/InputCheckbox';
+
 export { UserPhoto } from './UserPhoto';
-export { ContainerPage } from './ContainerPage';
 export { PagePath } from './PagePath';
 export { UserProfile } from './UserProfile';
+
+// Admin
+export { AdminSideBar } from './AdminSideBar';
+
+// User
+
+// exclude
+export { ContainerPage } from './ContainerPage';
