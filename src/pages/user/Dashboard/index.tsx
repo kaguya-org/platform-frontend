a8d964ee681b12dbb90593c156a649ef690c165c
Tiaguin061 <tiaguin180@gmail.com>
2022-04-03 19:02:29 -0300

    refactor: Dashboard has done

--- src/pages/user/Dashboard/index.tsx
+++ src/pages/user/Dashboard/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import { useModal } from '@tg0/react-modal';
 
 import {
@@ -29,23 +29,44 @@ export function Dashboard() {
 
   const seeMoreTrailsModal = useModal(false);
 
-  const [otherTrailsFromUser, setAllTrailsFromUser] = useState<UserType.ListAllTrailsFromUserResponse[]>([]);
-  const [otherTrails, setAllTrails] = useState<GlobalType.ListAllTrailsResponse[]>([]);
+  const [user_trails, setUserTrails] = useState<UserType.ListTrailFromUserResponse[]>([]);
+  const [other_trails, setOtherTrails] = useState<GlobalType.ListTrailsResponse[]>([]);
+
+  async function getUserTrails() {
+    try {
+      // TODO: Passar params take para pegar no máximo 3
+      const response = await api.user.trail.listTrailsFromUser();
+
+      setUserTrails(response.data);
+    } catch (error: any) {
+      console.log(error.message);
+    }
+  }
+
+  async function getTrails() {
+    try {
+      const response = await api.global.trail.list({
+        take: 6,
+        exclude_my_trails: true,
+        order: 'desc'
+      });
+
+      setOtherTrails(response.data);
+    } catch (error: any) {
+      console.log(error.message);
+    }
+  }
 
   useEffect(() => {
-    async function getAllUserTrails() {
-      try {
-        // TODO: Passar params take para pegar no máximo 3
-        const response = await api.user.trail.listAllTrailsFromUser();
-
-        setAllTrailsFromUser(response.data);
-      } catch (error: any) {
-        console.log(error.message);
-      }
-    }
-
-    getAllUserTrails();
+    getUserTrails();
+    getTrails();
   }, []);
+
+  const filter_user_trail = useMemo(() => {
+    return user_trails.filter(where => {
+      return where.user_trail.enabled;
+    });
+  }, [user_trails]);
 
   return (
     <S.Container>
@@ -61,6 +82,7 @@ export function Dashboard() {
             </div>
           </S.Welcome>
 
+          {/* TODO - create a history api */}
           <S.LastClasse to="#">
             <div className="last_classe_information">
               <img src={DEFAULT_TRAIL_IMAGE} alt="a" />
@@ -82,19 +104,19 @@ export function Dashboard() {
           <S.MyTrailsSection>
             <header>
               <h1> Minhas trilhas </h1>
-              {otherTrailsFromUser.length === 3 && (
-                <button type="button">Ver mais</button>
+              {user_trails.length === 3 && (
+                <button type="button">Ver todas</button>
               )}
             </header>
             <S.MyTrailsContainer>
-              {otherTrailsFromUser.map(({trail, progress}) => (
+              {filter_user_trail.map((trail) => (
                 <S.MyTrail to={`/trail/${trail.name}`} key={trail.id} >
                   <header>
                     <img src={trail.avatar_url || DEFAULT_TRAIL_IMAGE} alt={trail.name} />
                   </header>
                   <span>{trail.name}</span>
 
-                  <ProgressBar percent={progress} />
+                  <ProgressBar percent={trail.user_trail.progress} />
                 </S.MyTrail>
               ))}
             </S.MyTrailsContainer>
@@ -106,7 +128,7 @@ export function Dashboard() {
 
             <header className="other_trails_header">
               <h1 className="other_trails_header_title">Outras trilhas</h1>
-              {otherTrails.length > 0 && (
+              {other_trails.length === 6 && (
                 <button
                   className="see_more_trails"
                   type="button" 
@@ -118,15 +140,13 @@ export function Dashboard() {
             </header>
 
             <S.OtherTrailsContainer>
-              {otherTrails.map((trail) => (
+              {other_trails.map((trail) => (
                 <S.OtherTrail to={`/trail/${trail.id}`} key={trail.id} >
                   <img src={trail.avatar || DEFAULT_TRAIL_IMAGE} alt={trail.name} />
                   <div className="trail_information">
                     <h2 className="title">{trail.name}</h2>
-                    <span>
-                      {trail._count.playlists} playlists - 
-                      {trail._count.classes} aulas
-                    </span>
+                    <span>{trail._count.playlists} playlists - </span>
+                    <span>{trail._count.classes} aulas</span>
                   </div>
                 </S.OtherTrail>
               ))}
@@ -140,7 +160,7 @@ export function Dashboard() {
       <SeeMoreTrails 
         isOpen={seeMoreTrailsModal.state}
         handleClose={seeMoreTrailsModal.handleClose}
-        trails={otherTrails}
+        trails={other_trails}
       />
 
     </S.Container>
