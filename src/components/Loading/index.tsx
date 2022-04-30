fd3d508f9d3887827c97395e55f5c3c2df5e7dc1
Tiaguin061 <tiaguin180@gmail.com>
2022-04-30 13:21:50 -0300

    refactor: Changed size and add new props

--- src/components/Loading/index.tsx
+++ src/components/Loading/index.tsx
@@ -1,18 +1,21 @@
+import { HTMLAttributes } from 'react';
 import {
   LoadingContainer,
   CircleLoading,
   SquareLoading
 } from './styles';
 
-export type LoadingProps = {
-  size?: string;
-  type?: 'circle' | 'square';
+export type LoadingType = 'circle' | 'square';
+
+export type LoadingProps = HTMLAttributes<HTMLDivElement> & {
+  size?: number; // media "px"
+  type?: LoadingType;
 };
 
-export const Loading: React.FC<LoadingProps> = ({ type = 'circle', size = '20px', ...rest }) => {
+export const Loading: React.FC<LoadingProps> = ({ type = 'square', size = 20, ...rest }) => {
   const loadings = {
     circle: ( 
-      <LoadingContainer size={size}>
+      <LoadingContainer size={size} {...rest}>
         <CircleLoading />
       </LoadingContainer> 
     ),
